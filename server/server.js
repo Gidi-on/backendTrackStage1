b6ac6188babf2a0f68451e500c4ea2ac0655a062
Aribaba <86304459+Gidi-on@users.noreply.github.com>
2022-10-28 08:15:09 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const port = 7000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/home", (req, res) => {
+  console.log(req.body);
+  res.json({
+    slackUsername: "Aribaba",
+    backend: true,
+    age: 21,
+    bio: "Aspiring pro dev",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
